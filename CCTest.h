1097610e4487dbde40fddd11f1b68d2220ae6692
Ray Louie <raylouie@Raymonds-MacBook-Air-2.local>
2014-03-16 14:54:01 -0700

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Raymond Louie on 3/16/14.
+//  Copyright (c) 2014 Raymond Louie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Raymond Louie on 3/16/14.
+//  Copyright (c) 2014 Raymond Louie. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
